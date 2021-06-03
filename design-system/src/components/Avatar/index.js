46779f69229387aaa748f132fc32c2dd90a5caa2
nat-davydova <natti.davydova@gmail.com>
2021-06-03 12:02:21 +0300

    apply new token

--- design-system/src/components/Avatar/index.js
+++ design-system/src/components/Avatar/index.js
@@ -2,6 +2,8 @@ import { string } from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
+import {radius} from '../../tokens';
+
 /* Avatar
  *
  * Avatar displays user picture
@@ -11,7 +13,7 @@ const Avatar = ({ src, ...props }) => <StyledAvatar src={src} {...props} />;
 const StyledAvatar = styled.img`
   width: 48px;
   height: 48px;
-  border-radius: 16px;
+  border-radius: ${radius.radii16Radius};
 `;
 
 Avatar.propTypes = {
